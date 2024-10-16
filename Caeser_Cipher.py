e00bc4697506a3e6d019e6d70e217b2ec2eed201
ryangsling <jamesahsan1371@gmail.com>
2024-10-17 01:33:25 +0600

    first commit

---
+++ Caeser_Cipher.py
@@ -0,0 +1,50 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP^^^^^""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+                          88                                 
+           __             88                                 
+           ""             88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP^^^^^^" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(logo)
+will_continue = True
+while will_continue:
+    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift%=26
+
+
+    def Caeser_Cipher(cipher_text,shift_num,ciph_direction):
+        newtext=""
+        if ciph_direction=="decode":
+            shift_num *= -1
+        for char in cipher_text:
+            if char in alphabet:
+                position = alphabet.index(char)
+                new_position = position + shift_num
+                newtext += alphabet[new_position]
+            else:
+                newtext+=char
+        print(f"The {ciph_direction}d text is {newtext}")
+
+
+    Caeser_Cipher(text,shift,direction)
+    res = input("Type 'y' to run program again, type 'n' to end. ")
+    if res=='n':
+        will_continue=False
+        print("Exiting...")
+
